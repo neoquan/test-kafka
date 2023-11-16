3f2cd9e93549f89830758d243e9b0c936328e70c
neoquan <tungquanwork@gmail.com>
2023-11-16 10:22:01 +0700

    update code

--- start.sh
+++ start.sh
@@ -5,6 +5,16 @@
 
 # To use this .sh => ./start.sh
 
+NETWORK_NAME="cassandra-kafka"
+
+# Check if the network exists
+if ! docker network ls | awk '{print $2}' | grep -q "^$NETWORK_NAME$"; then
+    echo "Network $NETWORK_NAME does not exist. Creating it..."
+    docker network create "$NETWORK_NAME"
+else
+    echo "Network $NETWORK_NAME already exists. No action needed."
+fi
+
 # Build the Docker image
 docker compose up airflow-init
 
